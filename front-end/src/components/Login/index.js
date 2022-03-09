c47abee265196632bcee5cc131c40295ad83c7c9
Vinh Nguyen <vinhnq.ufm@gmail.com>
2022-03-09 16:50:38 +0700

    Add Login components

--- front-end/src/components/Login/index.js
+++ front-end/src/components/Login/index.js
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from "react";
 
 const Login = () => {
   return (
-    <div className="box">
+    <form className="box" method="POST">
       <p className="box-title">TODO APP</p>
-      <span className="box-decription">Enter your  to loggin</span>
+      <span className="box-decription">Enter your account to loggin</span>
       <div className="box-input">
-        <label for="userName">Username</label>
-        <input type="text" name="userName" id="userName"></input>
+        <label for="email">Email:</label>
+        <input type="text" name="email" id="email"></input>
       </div>
-    </div>
+      <div className="box-input">
+        <label for="password">Password:</label>
+        <input type="text" name="password" id="password"></input>
+      </div>
+      <div className="button-group">
+        <button className="button signin">Sign In</button>
+        <div className="link-group">
+          <a>Forgot password</a>
+          <span>
+            You don't have account? <a>Create new account</a>
+          </span>
+        </div>
+      </div>
+    </form>
   );
-}
+};
 
 export default Login;
-
